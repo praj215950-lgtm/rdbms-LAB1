7da4d6562bbabd5c3865787032d6790c153fa935
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 11:59:34 +0530

    Fix SQL syntax for Course table creation and insertion
    Corrected SQL syntax for creating the Course table and inserting records.

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,4 @@
--- Create Course table
-
--- Insert three records
-
--- Display structure
+use colleageDBBCA;
+CREATE TABLE Course (course_id VARCHAR(20),courseName VARCHAR(20),credite INT(20) PRIMERY KEY,DeparmentID VARCHAR(20);
+INSERT INTO Course (coruseID,CoruseName,Credite,DepartmentID) VALUES (10,"BCA",12,1);
+DESC Course;
