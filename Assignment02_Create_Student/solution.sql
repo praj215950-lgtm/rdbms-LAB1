84663684fb0e886ab86955f0c58d99068f357259
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 11:47:41 +0530

    Create Student table in solution.sql
    Create Student table with constraints and select query.

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,13 +1,3 @@
--- Create Student table
-
--- StudentID
-
--- StudentName
-
--- DOB
-
--- Gender
-
--- DepartmentID
-
--- Add constraints
+use colleagedbBCA;
+Create Table Student(Student_ID INT(5) PRIMERY KEY,Student VARCHAR(20,DOB Date GENDER VARCHAR(10) Department INT(5),CONTRAINT UQ_student UNIQUE(Student_Name);
+Select*from student
