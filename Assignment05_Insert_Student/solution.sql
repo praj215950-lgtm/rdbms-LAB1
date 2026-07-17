189cabb55c33dcec1aa375bfb7d498a3fead3b25
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 12:25:34 +0530

    Insert student records into the database

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,9 +1,3 @@
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+use colleageDBBCA;
+INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID) VALUES(1001,'Arun','Male',101),(1002,'Divya','female',102),(1003,'Karthik','Male',101);
+SELECT*FROM Student;
