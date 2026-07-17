a46d157940dfeb274df0a1b597f2a219d4c97980
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 12:30:01 +0530

    Fix SQL syntax and add additional course entries

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,4 +1,6 @@
 use colleageDBBCA;
 CREATE TABLE Course (course_id VARCHAR(20),courseName VARCHAR(20),credite INT(20) PRIMERY KEY,DeparmentID VARCHAR(20);
 INSERT INTO Course (coruseID,CoruseName,Credite,DepartmentID) VALUES (10,"BCA",12,1);
+INSERT INTO Coruse (coruseID,CoruseName,Credite,departmentID) VALUES (20,"BCA",13,2);
+INSERT INTO Coruse (courseID,CoruseName,Credite,departmentID) VALUES (30,"BCA",14,3);
 DESC Course;
