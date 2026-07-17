a809911c754dcb0932732da9a68beeafb9df0111
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 11:51:23 +0530

    Fix SQL syntax and modify Student table structure
    Corrected SQL syntax and added email and phone number columns to the Student table.

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,7 +1,3 @@
--- Alter Student table
-
--- Add Email
-
--- Add PhoneNumber
-
--- Display structure
+use colleageDBBCA;
+Create Table Student(student_id INT(5 PRIMERY KEY,Stduent_Name VARCHAR(20),after Table Student add(email VARCHAR(10),phone Number int(10));
+DESC Student
