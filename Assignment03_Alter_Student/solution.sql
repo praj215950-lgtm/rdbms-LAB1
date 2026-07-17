f385053258b9e91f580aee5db20517e5fe7356bc
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 12:39:16 +0530

    Fix SQL syntax errors in Student table definition
    Corrected SQL syntax for creating and altering the Student table.

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,3 +1,3 @@
-use colleageDBBCA;
-Create Table Student(student_id INT(5 PRIMERY KEY,Stduent_Name VARCHAR(20),after Table Student add(email VARCHAR(10),phone Number int(10));
-DESC Student
+alter table student
+add table student(30),
+add phonenumber varchar(10);
