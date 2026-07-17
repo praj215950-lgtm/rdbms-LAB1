d2dd7c376d89c9fedfe82415a095358efa714afd
praj215950-lgtm <praj215950@gmail.com>
2026-07-17 11:35:11 +0530

    Add department table and database creation SQL
    Created a new database and department table with relevant fields.

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,7 +1,4 @@
--- Write your SQL here
-
--- Create Database
-
--- Use Database
-
--- Create Department Table
+Create Table Collaegedb;
+use colleagedb;
+Create table Department(department_id INT PRIMERY KEY,department_Name VARCHAR(100),HOD VARCHAR(100));
+Select*From Department;
